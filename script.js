6cdcdc7d8e9464e4520ee89a31767ba1f4adbc13
MaryGorshkova <mint.mushrooom@gmail.com>
2015-08-07 18:07:32 +0300

    blueprint

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-window.onload = SetListener;
 
-function SetListener() {
-	DrawCounter();
-	
-	var button = getElementById("refresh_btn");
-	button.OnClick = DrawCounter;
-}
+document.addEventListener('DOMContentLoaded', DrawCounter)
 
 function DrawCounter() {
-	
+	var canv = document.getElementById('canvElement');
+	var gaugeField = canv.getContext('2d');
+	gaugeField.beginPath();
+	var x = canv.width*0.5;
+	var y = canv.height*0.9;
+	var r = canv.width*0.4;
+	gaugeField.arc(x,y,r,1*Math.PI,0*Math.PI);
+	var ri = r*0.8;
+	gaugeField.arc(x,y,ri,0*Math.PI,1*Math.PI, true);
+	gaugeField.closePath();
+	gaugeField.stroke();
 }
